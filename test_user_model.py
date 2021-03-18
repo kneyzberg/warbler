9a77669e03f358496a928112353643da80f377ad
Kathrin Neyzberg <kneyzberg@gmail.com>
2021-03-18 10:43:08 -0700

    create user test for repr, following, followers, and signup

--- test_user_model.py
+++ test_user_model.py
@@ -7,8 +7,9 @@
 
 import os
 from unittest import TestCase
+from app import IntegrityError
 
-from models import db, User, Message, Follows
+from models import db, User, Message, Follows, LikedMessage
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -33,17 +34,16 @@ app.config["TESTING"] = True
 USER_DATA = {
     "email": "minestrone@gmail.com",
     "username": "minestrone",
-    "bio": "I love is soup",
-    "location": "BAYYY AREAAAA"
-    "password": "password"
+    "password": "password",
+    "image_url": None,
 }
 
 USER_DATA2 = {
     "email": "bisque@gmail.com",
     "username": "bouillabaisse",
-    "bio": "soup or bust",
-    "location": "Soup Cove"
-    "password": "password"
+    "password": "password",
+    "image_url": "www.image.com"
+
 }
 
 
@@ -62,8 +62,11 @@ class UserModelTestCase(TestCase):
         db.session.add(user1)
         db.session.add(user2)
         db.session.commit()
+        db.session.rollback()
 
         self.client = app.test_client()
+        self.user1 = user1
+        self.user2 = user2
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -80,3 +83,66 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+    
+    def test_repr(self):
+        """Test Repr returns correct username and email """
+        repr_string = repr(User(email=self.user1.email,
+                                username=self.user1.username))
+        self.assertIn(f"{self.user1.username}, {self.user1.email}", repr_string)
+
+    def test_user_following(self):
+        """ Test that user following inlcludes user2 """
+        self.user1.following.append(self.user2)
+        db.session.commit()
+        self.assertIn(self.user2, self.user1.following)
+    
+    def test_user_not_following(self):
+        """Test that user1 is not folllowing user2"""
+        self.assertNotIn(self.user2, self.user1.following)
+    
+    def test_user_followers(self):
+        """Test user being followed by user2"""
+        self.user1.followers.append(self.user2)
+        db.session.commit()
+        self.assertIn(self.user2, self.user1.followers)
+
+    def test_user_not_follower(self):
+        """Test that user2 is not following user1"""
+        self.assertNotIn(self.user2, self.user1.followers)
+    
+    def test_successful_user_signup(self):
+        """Test user signup with valid credentials"""
+
+        new_user = User.signup(username="testname", 
+                               email="testing@gmail.com", 
+                               password="HASHED PASSWORD",
+                               image_url=None)
+        db.session.add(new_user)
+        db.session.commit()
+
+        user_query = User.query.filter_by(username="testname").first()
+        self.assertEqual(new_user.username, user_query.username)
+        self.assertEqual(new_user.email, user_query.email)
+        self.assertEqual(new_user.password, user_query.password)
+
+    def test_invalid_user_signup(self):
+        """Test User signup method fails with invalid credentials"""
+        try:
+            bad_user = User.signup(username="badtest", password="HASHED PASSWORD")
+        except TypeError:
+            signupError = "failure to assign user"
+    
+        self.assertEqual(signupError, "failure to assign user")
+    
+    def test_not_unique_user_signup(self):
+        """Test User signup method fails with invalid credentials"""
+        try:
+            duplicate_user = User.signup(username="minestrone",
+                                         email="minestrone@gmail.com",
+                                         password="HASHED PASSWORD",
+                                         image_url=None)
+        except IntegrityError:
+            duplicateError = "user already exists"
+    
+        self.assertEqual(duplicateError, "user already exists")
+    
